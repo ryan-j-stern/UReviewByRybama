da282ddfffccc2a0cd3854b7d6d5bb7aa6a795a1
baldwinsuen <suenbaldwin@gmail.com>
2019-12-07 16:51:16 -0500

    connecting to api with dummy api

--- ureview_mobile/lib/login.dart
+++ ureview_mobile/lib/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   @override
@@ -6,11 +9,33 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  String url = 'https://randomuser.me/api';
+
+  Future<String> makeRequest() async {
+    var response = await http
+      .get(Uri.encodeFull(url), headers: {'Accept': 'applicaton/json'});
+    print(response.body);
+    List data;
+    var extractData = json.decode(response.body);
+    data = extractData['results'];
+    print(data[0]['name']['first'] + ' ' + data[0]['name']['last']);
+    return (data[0]['name']['first'] + ' ' + data[0]['name']['last']);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
+      ),
+      body: new Center( 
+        child: RaisedButton(
+          child: Text('Make Request'),
+          onPressed: makeRequest,
+        ),
+        
       ),
     );
   }
